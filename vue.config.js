f55a1ec8f7170cfef15b0c88154db5929eaede49
ylf <1045438139@qq.com>
2019-05-22 23:15:34 +0800

    升级element admin的版本到4.2.0

--- vue.config.js
+++ vue.config.js
@@ -39,6 +39,7 @@ module.exports = {
       [process.env.VUE_APP_BASE_API]: {
         target: `http://new-api.boyuanziben.cn`,
         changeOrigin: true,
+        ws: false,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
         }
@@ -57,16 +58,6 @@ module.exports = {
     }
   },
   chainWebpack(config) {
-    const cdn = {
-      // inject tinymce into index.html
-      // why use this cdn, detail see https://github.com/PanJiaChen/tinymce-all-in-one
-      js: ['https://cdn.jsdelivr.net/npm/tinymce-all-in-one@4.9.3/tinymce.min.js']
-    }
-    config.plugin('html')
-      .tap(args => {
-        args[0].cdn = cdn
-        return args
-      })
     config.plugins.delete('preload') // TODO: need test
     config.plugins.delete('prefetch') // TODO: need test
 
